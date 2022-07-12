fcb1fb2d12c8c86c67fdb5a3424d7722cd334822
Vinicius Ferreira <viniciuferreira32@gmail.com>
2022-07-12 20:57:34 -0300

    Criado a rota post de user e o middleware

--- src/index.js
+++ src/index.js
@@ -8,5 +8,42 @@ app.use(cors())
 app.use(express.json())
 
 
+const users = []
+
+const checkUserNameExist = (req, res, next) => {
+  const { userName } = req.body
+
+ const user = users.find(user => user.userName === userName)
+
+  if (user) {
+    return res.status(400).send({error: 'UserName already exist'})
+  }
+
+  req.body = user
+
+  return next()
+}
+
+app.post('/user', (req, res) => {
+  const { name , userName } = req.body
+
+  const verifyUserNameExist = users.find(user => user.userName === userName)
+
+  if (verifyUserNameExist) {
+    return res.status(400).send({error: 'UserName already exist'})
+  }
+  
+  const user = {
+    id: uuidV4(),
+    name: name,
+    userName:userName,
+    todos:[]
+  }
+
+  users.push(user)
+
+  return res.status(201).json(user)
+})
+
 
 app.listen(3001)
